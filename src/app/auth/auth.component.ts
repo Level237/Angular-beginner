bf1fb66e9b6cfff8e5ed39784d8307d366985e42
level237 <level237>
2021-12-18 16:14:12 +0100

    Integration of the connection and disconnection module into the template

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+
+   authStatus:boolean;
+  constructor(private authservice:AuthService) { }
 
   ngOnInit(): void {
+    this.authStatus=this.authservice.isAuth;
   }
-
+  
+  onSignIn(){
+    this.authservice.signIn().then(
+      ()=>{
+        console.log('connexion reussi');
+        this.authStatus=this.authservice.isAuth;
+      }
+    );
+  }
+  onSignOut(){
+    this.authservice.signOut();
+    this.authStatus=this.authservice.isAuth;
+  }
 }
